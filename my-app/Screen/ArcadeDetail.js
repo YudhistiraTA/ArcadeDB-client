9db4cc6fb964d0a6ed6b8f749708d720da133133
bagastama97 <bagastama.putra97@gmail.com>
2023-06-30 23:13:48 +0700

    re-design

--- my-app/Screen/ArcadeDetail.js
+++ my-app/Screen/ArcadeDetail.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Dimensions, ScrollView, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import MapView from "react-native-maps";
 
 const { width, height } = Dimensions.get("window");
@@ -20,23 +28,54 @@ const ArcadeDetail = () => {
             longitudeDelta: 0.01,
           }}
         />
-        <View style={[styles.card, styles.bigCard]} />
-        <View style={styles.smallSquareContainer}>
-          <View style={styles.smallSquareRow}>
-            
+        <View style={[styles.card, styles.bigCard]}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.arcadeName}>Arcade Name</Text>
+            <View style={styles.rateContainer}>
+              <Text style={styles.rateText}>100 ☆☆☆☆☆</Text>
+            </View>
           </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.bookButton}>
+              <Text style={styles.buttonText}>Book</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.rateButton}>
+              <Text style={styles.rateButtonText}>Rate</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.userScheduleContainer}>
+            <Text style={styles.userScheduleText}>User Schedule</Text>
+            <View style={styles.circleContainer}>
+              <Image
+                source={require("../assets/image/user1.png")}
+                style={[styles.circle, styles.circle3]}
+              />
+              <Image
+                source={require("../assets/image/user2.png")}
+                style={[styles.circle, styles.circle2]}
+              />
+              <Image
+                source={require("../assets/image/user3.png")}
+                style={[styles.circle, styles.circle1]}
+              />
+              <Text style={{ marginLeft: 80 }}>
+                Didit and 8 users playing on 28 march 2023
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.smallSquareContainer}>
           <View style={[styles.smallSquareRow, styles.smallCardRow]}>
             <View style={[styles.smallCard, styles.smallCardMargin]}>
-              {/* Tambahkan komponen Image untuk menampilkan gambar */}
               <Image
-                source={require('../assets/image/imagesArcade.png')} 
+                source={require("../assets/image/imagesArcade.png")}
                 style={styles.smallCardImage}
               />
+              <Text>Game Name</Text>
             </View>
             <View style={[styles.smallCard, styles.smallCardMargin]}>
-              {/* Tambahkan komponen Image untuk menampilkan gambar */}
               <Image
-                source={require('../assets/image/imagesArcade.png')} 
+                source={require("../assets/image/imagesArcade.png")}
                 style={styles.smallCardImage}
               />
             </View>
@@ -66,13 +105,89 @@ const styles = StyleSheet.create({
     marginTop: 20,
     borderWidth: 2,
     borderColor: "black",
+    paddingHorizontal: 10,
+    paddingTop: 10,
   },
   bigCard: {
     marginBottom: 20,
   },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  arcadeName: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  rateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  rateText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  rateButton: {
+    backgroundColor: "#FDF3E6",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  rateButtonText: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  bookButton: {
+    backgroundColor: "#FDF3E6",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  userScheduleContainer: {
+    marginTop: 20,
+  },
+  userScheduleText: {
+    marginBottom: 10,
+  },
+  circleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  circle: {
+    width: 40,
+    height: 40,
+    borderRadius: 100,
+  },
+  circle1: {
+    position: "absolute",
+    top: -5,
+    left: 0,
+  },
+  circle2: {
+    position: "absolute",
+    top: -5,
+    left: 15,
+  },
+  circle3: {
+    position: "absolute",
+    top: -5,
+    left: 30,
+  },
   smallSquareContainer: {
     width: "100%",
-   
     flexDirection: "column",
     padding: 10,
   },
@@ -85,8 +200,8 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     backgroundColor: "#FDF3E6",
-    justifyContent: "center", // Pusatkan gambar secara vertikal
-    alignItems: "center", // Pusatkan gambar secara horizontal
+    justifyContent: "center",
+    alignItems: "center",
   },
   smallSquareImage: {
     width: 30,
@@ -107,14 +222,14 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "black",
     borderRadius: 50,
-    justifyContent: "center", // Pusatkan gambar secara vertikal
-    alignItems: "center", // Pusatkan gambar secara horizontal
+    justifyContent: "center",
+    alignItems: "center",
   },
   smallCardImage: {
-    width: 80, // Ubah ukuran width sesuai keinginan Anda
-    height: 80, // Ubah ukuran height sesuai keinginan Anda
+    width: 110,
+    height: 110,
     resizeMode: "cover",
-    borderRadius: 40, // Sesuaikan radius sesuai keinginan Anda
+    borderRadius: 100,
   },
   smallCardMargin: {
     margin: 5,
--- my-app/Screen/HomeScreen.js
+++ my-app/Screen/HomeScreen.js
@@ -11,8 +11,17 @@ import { Ionicons } from "@expo/vector-icons";
 
 // Import image
 import arcadeImage from "../assets/image/imagesArcade.png";
+import { PressStart2P_400Regular } from "@expo-google-fonts/press-start-2p";
+import { useFonts } from "expo-font";
 
 function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    PressStart2P_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
@@ -28,67 +37,73 @@ function HomeScreen() {
       </View>
       <View style={styles.squareContainer}>
         <View style={[styles.columnContainer, { marginHorizontal: 10 }]}>
-          <View style={styles.square}></View>
-          <View style={styles.square}></View>
+          <View style={styles.square}>
+            <View
+              style={[styles.iconContainer, { backgroundColor: "#E4B84C" }]}
+            >
+              <Ionicons name="game-controller" size={24} color="#FFFFFF" />
+            </View>
+            <Text style={styles.squareText}>Games</Text>
+          </View>
+          <View style={styles.square}>
+            <View
+              style={[styles.iconContainer, { backgroundColor: "#EBB3C3" }]}
+            >
+              <Ionicons name="chatbubbles-outline" size={24} color="#FFFFFF" />
+            </View>
+            <Text style={styles.squareText}>Messages</Text>
+          </View>
         </View>
         <View style={[styles.columnContainer, { marginHorizontal: 10 }]}>
-          <View style={styles.square}></View>
-          <View style={styles.square}></View>
+          <View style={styles.square}>
+            <View
+              style={[styles.iconContainer, { backgroundColor: "#9DBDFB" }]}
+            >
+              <Ionicons name="rocket-outline" size={24} color="#FFFFFF" />
+            </View>
+            <Text style={styles.squareText}>Arcades</Text>
+          </View>
+          <View style={styles.square}>
+            <View
+              style={[styles.iconContainer, { backgroundColor: "#81ADB4" }]}
+            >
+              <Ionicons name="people-outline" size={24} color="#FFFFFF" />
+            </View>
+            <Text style={styles.squareText}>Followers</Text>
+          </View>
         </View>
       </View>
-
-      <ScrollView style={styles.carouselContainer}>
+      <Text style={{ fontFamily: "PressStart2P_400Regular", marginBottom: 10 }}>
+        Recomendations
+      </Text>
+      <ScrollView>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
           <View style={[styles.card, { marginBottom: 30 }]}>
             <Image source={arcadeImage} style={styles.cardImage} />
             <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 1</Text>
+              <Text style={styles.cardText}>The Breeze, Taangerang</Text>
+              <Text style={styles.cardText}>☆☆☆☆☆</Text>
             </View>
           </View>
           <View style={[styles.card, { marginBottom: 30 }]}>
             <Image source={arcadeImage} style={styles.cardImage} />
             <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 2</Text>
+              <Text style={styles.cardText}>The Breeze, Taangerang</Text>
+              <Text style={styles.cardText}>☆☆☆☆☆</Text>
             </View>
           </View>
           <View style={[styles.card, { marginBottom: 30 }]}>
             <Image source={arcadeImage} style={styles.cardImage} />
             <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 3</Text>
+              <Text style={styles.cardText}>The Breeze, Taangerang</Text>
+              <Text style={styles.cardText}>☆☆☆☆☆</Text>
             </View>
           </View>
           <View style={[styles.card, { marginBottom: 30 }]}>
             <Image source={arcadeImage} style={styles.cardImage} />
             <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 4</Text>
-            </View>
-          </View>
-        </ScrollView>
-      </ScrollView>
-      <ScrollView style={styles.carouselContainer}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <View style={[styles.card, { marginBottom: 30 }]}>
-            <Image source={arcadeImage} style={styles.cardImage} />
-            <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 1</Text>
-            </View>
-          </View>
-          <View style={[styles.card, { marginBottom: 30 }]}>
-            <Image source={arcadeImage} style={styles.cardImage} />
-            <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 2</Text>
-            </View>
-          </View>
-          <View style={[styles.card, { marginBottom: 30 }]}>
-            <Image source={arcadeImage} style={styles.cardImage} />
-            <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 3</Text>
-            </View>
-          </View>
-          <View style={[styles.card, { marginBottom: 30 }]}>
-            <Image source={arcadeImage} style={styles.cardImage} />
-            <View style={styles.cardContent}>
-              <Text style={styles.cardText}>Card 4</Text>
+              <Text style={styles.cardText}>The Breeze, Taangerang</Text>
+              <Text style={styles.cardText}>☆☆☆☆☆</Text>
             </View>
           </View>
         </ScrollView>
@@ -118,6 +133,7 @@ const styles = StyleSheet.create({
   searchInput: {
     flex: 1,
     fontSize: 16,
+    fontFamily: "PressStart2P_400Regular",
   },
   searchIcon: {
     marginLeft: 10,
@@ -132,38 +148,49 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   square: {
+    flexDirection: "row",
+    alignItems: "center",
     width: "100%",
     height: 70,
     backgroundColor: "#FFFFFF",
     borderRadius: 50,
     marginBottom: 10,
+    paddingHorizontal: 10,
   },
-  carouselContainer: {
-    marginBottom: 20,
+  iconContainer: {
+    borderRadius: 50,
+    padding: 8,
+    marginRight: 10,
+  },
+  squareText: {
+    fontSize: 8,
+    color: "#000000",
+    fontFamily: "PressStart2P_400Regular",
   },
   card: {
     width: 300,
     height: 200,
-    borderRadius: 30,
+    borderRadius: 15,
     backgroundColor: "#FFFFFF",
     marginRight: 10,
-    elevation: 2,
+    // elevation: 2,
   },
   cardImage: {
     width: "100%",
     height: "70%",
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
   },
   cardContent: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    marginLeft: 10,
   },
   cardText: {
-    fontSize: 18,
-    fontWeight: "bold",
+    marginTop: 10,
+    fontSize: 12,
+    // fontWeight: "bold",
     color: "#000000",
+    fontFamily: "PressStart2P_400Regular",
   },
 });
 
